25610d5bad9958d3cb874f633a82064ea1afb705
TrueDeva <truedeva@gmail.com>
2023-02-18 20:39:12 +0100

    Prototype
    exposed API key removed
    additional checks and error handling added in case of erronoius input
    Better design
    
    Signed-off-by: TrueDeva <truedeva@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,27 +3,22 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-    // Declare state variables
-    const [word, setWord] = useState(''); // the input word from the form
-    const [synonyms, setSynonyms] = useState([]); // the list of synonyms returned from the API
-    const [errorMessage, setErrorMessage] = useState(''); // an error message to display if there is a problem with the input
+    const [word, setWord] = useState('');
+    const [synonyms, setSynonyms] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    // Handle input change in the form
     const handleInputChange = (e) => {
         setWord(e.target.value);
     };
 
-    // Handle form submission
     const handleFormSubmit = async (e) => {
         e.preventDefault();
         if (word.trim() === '') {
-            // If the input is empty, set the error message and clear the list of synonyms
             setErrorMessage('Please enter a word.');
             setSynonyms([]);
             return;
         }
         if (word.trim().split(' ').length > 1) {
-            // If the input contains multiple words, set the error message and clear the list of synonyms
             setErrorMessage('Please enter only one word at a time.');
             setSynonyms([]);
             return;
@@ -39,7 +34,6 @@ function App() {
         try {
             const response = await axios.request(options);
             if (response.data.synonyms.length === 0) {
-                // If no synonyms are found, set the error message and clear the list of synonyms
                 setErrorMessage('No synonyms found, try again with another word.');
                 setSynonyms([]);
                 return;
@@ -55,7 +49,6 @@ function App() {
         <div className="container">
             <h1>Synonym App</h1>
             {errorMessage && word.trim() !== '' && (
-                // If there is an error message and the input is not empty, display the error message in red text
                 <p className="text-danger">{errorMessage}</p>
             )}
             <form onSubmit={handleFormSubmit}>
@@ -73,18 +66,16 @@ function App() {
                 </button>
             </form>
             {synonyms.length > 0 ? (
-                // If there are synonyms, display the list of synonyms
                 <ul>
                     {synonyms.map((synonym) => (
                         <li key={synonym}>{synonym}</li>
                     ))}
                 </ul>
             ) : (
-                // If there are no synonyms, display the error message in bold italics
-                word.trim() !== '' && <p><em>No synonyms found, try again with another word.</em></p>
+                word.trim() !== '' &&
+                <p className="no-synonyms"><em>No synonyms found, try again with another word.</em></p>
             )}
         </div>
     );
 }
-
 export default App;
