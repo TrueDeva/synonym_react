4d1acacc5f747564df9b027c211e27f6b967873e
TrueDeva <truedeva@gmail.com>
2023-02-18 20:27:35 +0100

    Prototype
    exposed API key removed
    additional checks and error handling added in case of erronoius input
    
    Signed-off-by: TrueDeva <truedeva@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,26 +3,49 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-    const [word, setWord] = useState('');
-    const [synonyms, setSynonyms] = useState([]);
+    // Declare state variables
+    const [word, setWord] = useState(''); // the input word from the form
+    const [synonyms, setSynonyms] = useState([]); // the list of synonyms returned from the API
+    const [errorMessage, setErrorMessage] = useState(''); // an error message to display if there is a problem with the input
 
+    // Handle input change in the form
     const handleInputChange = (e) => {
         setWord(e.target.value);
     };
 
+    // Handle form submission
     const handleFormSubmit = async (e) => {
         e.preventDefault();
+        if (word.trim() === '') {
+            // If the input is empty, set the error message and clear the list of synonyms
+            setErrorMessage('Please enter a word.');
+            setSynonyms([]);
+            return;
+        }
+        if (word.trim().split(' ').length > 1) {
+            // If the input contains multiple words, set the error message and clear the list of synonyms
+            setErrorMessage('Please enter only one word at a time.');
+            setSynonyms([]);
+            return;
+        }
         const options = {
             method: 'GET',
             url: `https://wordsapiv1.p.rapidapi.com/words/${word}/synonyms`,
             headers: {
-                'X-RapidAPI-Key': 'bb565e34a7msh628bbdf2ad52d40p1f9e08jsn3bc9545b5091',
+                'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
                 'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
             }
         };
         try {
             const response = await axios.request(options);
+            if (response.data.synonyms.length === 0) {
+                // If no synonyms are found, set the error message and clear the list of synonyms
+                setErrorMessage('No synonyms found, try again with another word.');
+                setSynonyms([]);
+                return;
+            }
             setSynonyms(response.data.synonyms.slice(0, 10));
+            setErrorMessage('');
         } catch (error) {
             console.error(error);
         }
@@ -31,6 +54,10 @@ function App() {
     return (
         <div className="container">
             <h1>Synonym App</h1>
+            {errorMessage && word.trim() !== '' && (
+                // If there is an error message and the input is not empty, display the error message in red text
+                <p className="text-danger">{errorMessage}</p>
+            )}
             <form onSubmit={handleFormSubmit}>
                 <div className="form-group">
                     <input
@@ -45,12 +72,16 @@ function App() {
                     Generate Synonyms
                 </button>
             </form>
-            {synonyms.length > 0 && (
+            {synonyms.length > 0 ? (
+                // If there are synonyms, display the list of synonyms
                 <ul>
                     {synonyms.map((synonym) => (
                         <li key={synonym}>{synonym}</li>
                     ))}
                 </ul>
+            ) : (
+                // If there are no synonyms, display the error message in bold italics
+                word.trim() !== '' && <p><em>No synonyms found, try again with another word.</em></p>
             )}
         </div>
     );
